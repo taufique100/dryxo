82e336b6b794894cc8c2ad21411d6fe14197c061
Taufique-Speqto <taufique.ali@speqto.com>
2025-09-19 17:34:25 +0530

    code

--- src/Pages/Home/Sweeper/Sweeper.jsx
+++ src/Pages/Home/Sweeper/Sweeper.jsx
@@ -9,32 +9,19 @@ const Sweeper = () => {
   return (
     <>
       <Carousel fade interval={3000} indicators={true} controls={true} className='mb-5 '>
-        {/* Slide 1 */}
-        <Carousel.Item>
-          <img
-            className="d-block w-100 slider-img"
-            src={Sweeper2}
-            alt="First slide"
-          />
-        </Carousel.Item>
+        {
+          [Sweeper2, Sweeper1, Sweeper3]?.map((imgItems, idx) => (
 
-        {/* Slide 2 */}
-        <Carousel.Item>
-          <img
-            className="d-block w-100 slider-img"
-            src={Sweeper1}
-            alt="Second slide"
-          />
-        </Carousel.Item>
+            <Carousel.Item key={idx}>
+              <img
+                className="d-block w-100 slider-img"
+                src={imgItems}
+                alt="First slide"
+              />
+            </Carousel.Item>
+          ))
+        }
 
-        {/* Slide 3 */}
-        <Carousel.Item>
-          <img
-            className="d-block w-100 slider-img"
-            src={Sweeper3}
-            alt="Third slide"
-          />
-        </Carousel.Item>
       </Carousel>
     </>
   );
